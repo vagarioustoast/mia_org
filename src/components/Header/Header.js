9fb1cfbe26252e459d4ae8f66e3d8c709928ed10
vagarioustoast <vagarioustoast@gmail.com>
2019-04-01 13:24:24 -0700

    adds some header content

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Marxist Internet Archive</h1>
+        <a>Sign in</a>
+        <a>Register</a>
+        <nav>
+          <ul>
+            <li>First International</li>
+            <li>Western Marxism</li>
+            <li>Trotskyists</li>
+            <li>Left Communism</li>
+            <li>African Liberation</li>
+            <li>Black Liberation</li>
+            <li>Feminists</li>
+            <li>Philosophy</li>
+            <li>Ethics</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
