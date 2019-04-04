5c8300e7b1824102eba947cd83fd2e3208a88538
vagarioustoast <vagarioustoast@gmail.com>
2019-04-04 13:04:55 -0700

    adds linking to article from category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default class Category extends Component {
@@ -10,7 +11,7 @@ export default class Category extends Component {
             <div className="aspect-ratio aspect-ratio--1x1">
               <img className="db bg-center cover aspect-ratio--object" />
             </div>
-            <a href="#0" className="ph2 ph0-ns pb3 link db">
+            <a href="/articles/article/:id" className="ph2 ph0-ns pb3 link db">
               <h3 className="f5 f4-ns mb0 black-90">Title of piece</h3>
               <h3 className="f6 f5 fw4 mt2 black-60">Author of piece</h3>
             </a>
