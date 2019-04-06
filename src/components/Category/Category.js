b1b842af7a3fe61e7737ca01778146a25eb47978
vagarioustoast <vagarioustoast@gmail.com>
2019-04-06 12:58:44 -0700

    adds api call for categories

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,31 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 export default class Category extends Component {
   render() {
     return (
-      <div className="category-container">
-        <article className="pv4 bt bb b--black-10 ph3 ph0-l">
-          <div className="flex flex-column flex-row-ns">
-            <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-              <h1 className="f3 athelas mt0 lh-title">
-                Tech Giant Invests Huge Money to Build a Computer Out of Science
-                Fiction
-              </h1>
-              <p className="f5 f4-l lh-copy athelas">
-                The tech giant says it is ready to begin planning a quantum
-                computer, a powerful cpu machine that relies on subatomic
-                particles instead of transistors.
-              </p>
-            </div>
-          </div>
-          <p className="f6 lh-copy gray mv0">
-            By <span className="ttu">author name</span>
-          </p>
-          <time className="f6 db gray">pubdate</time>
-        </article>
-      </div>
+      <div className="category-container">{this.props.categoryArticles}</div>
     );
   }
 }
--- src/components/Category/CategoryContainer.js
+++ src/components/Category/CategoryContainer.js
@@ -17,11 +17,30 @@ export default class CategoryContainer extends Component {
   }
 
   render() {
+    const { articles } = this.state;
+    const categoryArticles = articles.map(article => {
+      return (
+        <article className="pv4 bt bb b--black-10 ph3 ph0-l" key={article._id}>
+          <div className="flex flex-column flex-row-ns">
+            <div className="w-100 w-90-ns pr3-ns order-2 order-1-ns">
+              <h1 className="f3 athelas mt0 lh-title">{article.title}</h1>
+              <p className="f5 f4-l lh-copy athelas">
+                {article.content.slice(0, 200)}...
+              </p>
+            </div>
+          </div>
+          <p className="f6 lh-copy gray mv0">
+            By <span className="ttu">{article.author.name}</span>
+          </p>
+          <time className="f6 db gray">{article.pubDate}</time>
+        </article>
+      );
+    });
     return (
       <div>
         <section className="mw7 center">
           <h2 className="athelas ph3 ph0-l">News</h2>
-          <Category />
+          <Category categoryArticles={categoryArticles} />
         </section>
       </div>
     );
