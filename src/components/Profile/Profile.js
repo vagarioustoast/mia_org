0a8629fb688fe9302a5fcde571e9e8e54465d95e
vagarioustoast <vagarioustoast@gmail.com>
2019-04-11 10:14:19 -0700

    adds edit form to user profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -16,9 +16,40 @@ export default class Profile extends Component {
             alt="user"
           />
         </figure>
-        <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">
-          Edit
-        </a>
+        {/* Edit Profile Information */}
+        <h4> Edit your Profile </h4>
+        <form
+          className="pa4 black-80"
+          method="POST"
+          action={`http://localhost:3001/users/update/${user._id}`}
+        >
+          <label>
+            <input
+              className="input-reset ba b--black-20 pa2 mb2 db w-100"
+              placeholder={user.displayName}
+            />
+          </label>
+          <label>
+            <input
+              className="input-reset ba b--black-20 pa2 mb2 db w-100"
+              placeholder={user.avatarUrl ? user.avatarUrl : "Avatar url"}
+            />
+          </label>
+          <label>
+            <textarea
+              className="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2"
+              placeholder={
+                user.description ? user.description : "Describe yourself"
+              }
+            />
+          </label>
+          <button
+            className="f6 link dim ph3 pv2 mb2 dib white bg-black"
+            href="#0"
+          >
+            Edit
+          </button>
+        </form>
         <h2>{user.displayName}</h2>
         <p>{user.description}</p>
         <div className="annotations">
